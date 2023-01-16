2b1c5b7625e476a6fdc09f8ec145a2b9a8369469
Michael King <michaelj.king@btinternet.com>
2023-01-16 13:14:20 +0000

    Basic chart plotting along the reals

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -1,6 +1,7 @@
 ﻿open ComplexNumbers
 open ZetaFunction
-open FSharp.Charting
+open Plotly.NET
+open System
 
 let s = 
     {
@@ -8,6 +9,27 @@ let s =
         Imaginary = 5
     }
 
-let data = [ for x in 1.0 .. 10.0 -> x, (ZetaFunction.approximateZeta (Number.real x) 3)]
+//let data = [ for x in 1.0 .. 10.0 -> x, (ZetaFunction.approximateZeta (Number.real x) 3)]
+//
+//Chart.Line data
 
-Chart.Line data
+let two = (ZetaFunction.approximateZeta (Number.real 2) 100)
+let three = (ZetaFunction.approximateZeta (Number.real 3) 100)
+let four = (ZetaFunction.approximateZeta (Number.real 4) 100)
+
+let point3d = 
+    Chart.Point3D(
+        [
+            2, two.Real, two.Imaginary
+            3, three.Real, three.Imaginary
+            4, four.Real, four.Imaginary
+        ],
+        MultiText = ["A"; "B"; "C"],
+        TextPosition = StyleParam.TextPosition.BottomCenter
+    )
+    |> Chart.withXAxisStyle("S as non-complex", Id=StyleParam.SubPlotId.Scene 1) // in contrast to 2D plots, x and y axes of 3D charts have to be set via the scene object
+    |> Chart.withYAxisStyle("Re(z(s))", Id=StyleParam.SubPlotId.Scene 1) // in contrast to 2D plots, x and y axes of 3D charts have to be set via the scene object
+    |> Chart.withZAxisStyle("Im(z(s))")
+    |> Chart.withSize(800.,800.)
+
+point3d |> Chart.show
